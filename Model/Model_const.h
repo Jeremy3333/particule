7634906412373fe036155843e554cf889a1c25ef
Jeremy3333 <jeremy.simond220801@gmail.com>
2024-03-17 04:35:40 +0100

    Created a Model_const.h to avoid magic numbers.

---
+++ Model/Model_const.h
@@ -0,0 +1,15 @@
+//
+// Created by tally on 17/03/24.
+//
+
+#ifndef MODEL_CONST_H
+#define MODEL_CONST_H
+
+#include "../Controller/Controller_const.h"
+
+#define WORLD_WIDTH (GENERAL_WIDTH / ZOOM_FACTOR)
+#define WORLD_HEIGHT (GENERAL_HEIGHT / ZOOM_FACTOR)
+
+#define PARTICLE_RADIUS 5
+
+#endif //MODEL_CONST_H
