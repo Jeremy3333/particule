ed9c7c8106be389b41135f7e5c52fd7f9cbaaeea
Jeremy3333 <jeremy.simond220801@gmail.com>
2024-03-17 04:32:01 +0100

    Added a Controller_const.h that can be used by SDL_const.h Model_const.h to create link constants.

---
+++ Controller/Controller_const.h
@@ -0,0 +1,12 @@
+//
+// Created by tally on 17/03/24.
+//
+
+#ifndef CONTROLLER_CONST_H
+#define CONTROLLER_CONST_H
+
+#define GENERAL_WIDTH 640
+#define GENERAL_HEIGHT 480
+#define ZOOM_FACTOR 1
+
+#endif //CONTROLLER_CONST_H
